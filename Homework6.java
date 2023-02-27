3efa413077f7bae7996b0bf3f3f1c2f4c4bc74a3
Natalia <31250744+allwdesign@users.noreply.github.com>
2023-02-28 00:46:52 +0300

    Add solutions for Homework6 tasks

---
+++ Homework6.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+import java.util.Iterator;
+
+
+public class Homework6 {
+    // Develop a program that mimics the behavior of a HashSet collection.
+    // In the program, create an add method that adds an element, the toString method
+    // returning a string with set elements and a method that allows
+    // read elements by index. The data format is Integer.
+
+    public static void main(String[] args) {
+        
+        MyHashSet myHashSet = new MyHashSet();
+
+        myHashSet.add(1);
+        myHashSet.add(6);
+        myHashSet.add(37);
+        myHashSet.add(6);
+
+        System.out.println(myHashSet);  // 1 37 6 
+
+        System.out.println(myHashSet.readElementsByIndex(9));  // default
+        System.out.println(myHashSet.readElementsByIndex(37));  // Object hash 36f0f1be
+
+    }
+
+}
+
+class MyHashSet {
+    private HashMap<Integer, Object> map = new HashMap<>();
+    private final Object OBJ = new Object();
+
+    public boolean add(Integer i) {
+        return map.put(i, OBJ) != null ? true : false;
+    }
+
+    @Override
+    public String toString() {
+        String resultStr = "";
+        // map.keySet() returns a Set view of the keys contained in this map.
+        Iterator<Integer> iterator = map.keySet().iterator();
+
+        while (iterator.hasNext()) {
+            resultStr += iterator.next().toString() + " ";
+        }
+        return resultStr;
+    }
+    
+    public Object readElementsByIndex(Integer i) {
+        return map.getOrDefault(i, "default");
+    }
+}
